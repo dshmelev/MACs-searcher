3c2563562d0f9dfb4613f5d3e44bd3d3ccec638c
Dmitry Shmelev <avikez@gmail.com>
2015-09-27 22:36:36 +0300

    Init commit

---
+++ app/models/record.rb
@@ -0,0 +1,34 @@
+class Record < ActiveRecord::Base
+  self.table_name = "history_text"
+  default_scope { order('id DESC') }
+  has_one :item, :primary_key => "itemid", :foreign_key => "itemid" 
+  has_one :host, :through => :item
+  validates :item, presence: true
+  validates :host, presence: true
+
+  def self.find_by_value (value)
+    self.where('value like ?', "%#{value}%").
+         first
+  end
+  def next
+    rec = self.class.where('value = ?', value).
+                     where('itemid <> ?',  itemid).
+                     where('id < ?',       id).first
+    raise ActiveRecord::RecordNotFound if rec.nil?
+    return rec
+  end
+  def prev
+    rec = self.class.where('value = ?', value).
+                     where('itemid <> ?',  itemid).
+                     where('id > ?',       id).first
+    raise ActiveRecord::RecordNotFound if rec.nil?
+    return rec
+  end
+end
+
+class Item < ActiveRecord::Base
+  self.inheritance_column = :_type_disabled
+  has_one :host, :primary_key => "hostid", :foreign_key => "hostid"
+end
+class Host < ActiveRecord::Base
+end
